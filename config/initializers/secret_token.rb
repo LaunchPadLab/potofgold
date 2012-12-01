5cee31742f26bed952a56c869659cc64df9bc831
bhennes2 <bhennes2@gmail.com>
2012-12-01 11:56:45 -0600

    Added PG gem for Heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Potofgold::Application.config.secret_token = '84ce2e0af3905a3e0c0dee9c2436620e3eacdc26a626136492fdfc08105ab93bd84a8533d55b2ab345903657b32b17fd7f2014dea092a1c436904849655ce177'
