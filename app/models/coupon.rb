0f246473644755ee29ec8243d608b36ac8341d99
bhennes2 <bhennes2@gmail.com>
2012-12-06 16:56:21 -0600

    Show correct graphing of followers

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -9,6 +9,7 @@ class Coupon < ActiveRecord::Base
   scope :redeemed, where(redeemed: true)
   scope :for_user, lambda { |user_id| where(user_id: user_id) }
   scope :referrals, where(referred: true)
+  scope :oldest, order('created_at ASC')
   
   #Instance Method
   def not_redeemed?
