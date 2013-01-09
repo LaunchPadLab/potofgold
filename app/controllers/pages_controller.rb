d72ec347bc7dd916791ce483563a32c270ce5bc8
bhennes2 <bhennes2@gmail.com>
2013-01-09 08:36:02 -0600

    Show latest deal on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,14 @@
 class PagesController < ApplicationController
 
   def home
-	if current_authorized_user
-		if current_authorized_user.class.name.include?("Advertiser")
-			redirect_to deals_path
-		else
-			redirect_to coupons_path
-		end
-	end
+    @deal = Deal.order('updated_at DESC').first
+  	if current_authorized_user
+  		if current_authorized_user.class.name.include?("Advertiser")
+  			redirect_to deals_path
+  		else
+  			redirect_to coupons_path
+  		end
+  	end
   end
 
 end
